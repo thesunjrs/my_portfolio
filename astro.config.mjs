3bc0de17b06d089fc961631fe1840b4b47863630
thesunjrs <aditya.s2021btcseai@srisriuniversity.edu.in>
2023-09-03 00:05:07 +0530

    introduced to keystatic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import preact from "@astrojs/preact";
+
+import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [preact(), react(), markdoc()]
 });
